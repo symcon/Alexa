ec0f5080c76f0f46b98e469548f68fe71b41c783
DrNiels <nt@ip-symcon.de>
2018-08-07 07:58:22 +0000

    Apply fixes from StyleCI

--- Alexa/registry.php
+++ Alexa/registry.php
@@ -211,15 +211,14 @@ class DeviceTypeRegistry
         ];
 
         foreach (self::$supportedDeviceTypes as $deviceType) {
-            foreach(call_user_func(self::classPrefix . $deviceType . '::getTranslations') as $language => $languageTranslations) {
+            foreach (call_user_func(self::classPrefix . $deviceType . '::getTranslations') as $language => $languageTranslations) {
                 if (array_key_exists($language, $translations)) {
                     foreach ($languageTranslations as $original => $translated) {
                         if (array_key_exists($original, $translations[$language])) {
                             if ($translations[$language][$original] != $translated) {
                                 throw new Exception('Different translations ' . $translated . ' + ' . $translations[$language][$original] . ' for original ' . $original . ' was found!');
                             }
-                        }
-                        else {
+                        } else {
                             $translations[$language][$original] = $translated;
                         }
                     }
